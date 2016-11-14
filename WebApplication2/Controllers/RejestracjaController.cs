7aae3ec724e72370a74ea7f1442d8dcd86adeeef
czarny1000 <bonek11@gmail.com>
2016-11-14 22:58:10 +0100

    commit

---
+++ WebApplication2/Controllers/RejestracjaController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class RejestracjaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Rejestracja");
+        }
+        // GET: Rejestracja
+        public ActionResult Rejestracja(AspNetUser user)
+        {
+            //return View();
+            if (ModelState.IsValid)
+            {
+                //List<AspNetUsers> userDod = new List<AspNetUsers>();
+                //  userDod.Insert(new AspNetUsers {Name = use.Imie, Nazwisko = model.Nazwisko, Miasto = model.Miasto, KodPocztowy = model.KodPocztowy, Adres = model.Adres, Email = model.Email, Telefon = model.Telefon });
+                Entities2 db = new Entities2();
+                if (db.AspNetUsers.Any())
+                {
+                    int id = db.AspNetUsers.OrderByDescending(p => p.Id).Select(p => p.Id).First();
+                        //user.Id = userr + 1;
+                        //var update = dc.UserProfile.First(d => d.UserName == User.Identity.Name);
+                        //var upadte = db.AspNetUserss.First(d => d.Email == "test");
+                        //   var update2 = db.AspNetUsers.First(d => d.Confirm == 0);
+                        //     update2.Email = "test2";
+                        user.Id = id + 1;
+                }
+                else
+                    user.Id = 1;
+                db.AspNetUsers.Add(user);
+                db.SaveChanges();
+            }
+            ViewBag.Message = user.Name + "  " + user.Surname + " zarejestrowano poprawnie";
+            return View();
+        } 
+    }
+}
---
+++ WebApplication2/Models/RejestracjaModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class RejestracjaModel
+    {
+        public class Koszyk
+        {
+            [Required]
+            public string Email { get; set; }
+            [Required]
+            public string Name { get; set; }
+            [Required]
+            public string Surname { get; set; }
+            [Required]
+            public int Pesel { get; set; }
+            [Required]
+            public string Department { get; set; }
+            [Required]
+            public string PasswordHash { get; set; }
+            [Required]
+            public int PhoneNumber { get; set; }
+            [Required]
+            public System.DateTime BirthDate { get; set; }
+            [Required]
+            public string UserName { get; set; }
+        }
+    }
+}
